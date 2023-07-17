65dd0413523a384bda7e796897c46b7c730ff55e
Farahrama <rafarah507@gmail.com>
2023-07-17 14:49:47 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: prints all single digit numbers of base 10 starting from 0
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
