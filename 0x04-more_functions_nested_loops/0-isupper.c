dc5319ea6838ee8c4e43b00ec950d1b55cfb6726
Farahrama <rafarah507@gmail.com>
2023-07-20 17:30:35 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  *_isupper - check if c is upper
- *
- * @c: input for alphabet
- *
+ *@c: input for alphabet
  *Return: 1 if c is uppercase Return 0 if not
 */
 int _isupper(int c);
