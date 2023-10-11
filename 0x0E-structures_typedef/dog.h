24b5589cb2f9cf1a3f42dc92c8dad1066d845eba
Farahrama <rafarah507@gmail.com>
2023-10-10 18:30:38 -0700

    farah

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #ifndef _DOG_H
 #define _DOG_H
+
 /**
  * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  *
- * Description: just alone dog in a big kitty world 
+ * Description: just alone dog struct in a big kitty world 
  */
 struct dog
 {
@@ -14,4 +15,5 @@ char *name;
 float age;
 char *owner;
 };
+
 #endif
