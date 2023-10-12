54c1e292cc06a3fa0e6642e8ee3754bc76987308
Farahrama <rafarah507@gmail.com>
2023-10-12 13:34:36 -0700

    farah

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 int i = n, sum = 0;
 va_list args;
 if (!n)
-return(0);
+return (0);
 va_start(args, n);
 while (i--)
 sum += va_arg(args, int);
 va_end(args);
-return(sum);
+return (sum);
 }
