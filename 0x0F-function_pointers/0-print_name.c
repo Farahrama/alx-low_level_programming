5cf3455a4450fb042d8b0ae90c9f2e119779a5cb
Farahrama <rafarah507@gmail.com>
2023-10-11 18:08:53 -0700

    farah

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name_as_is - prints a name
+ * @name: name of the person
+ * Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if(name && f)
+f(name);
+}
