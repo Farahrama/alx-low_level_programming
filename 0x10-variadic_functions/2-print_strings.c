8e8ef399256b1a85a91e02bbac08113d321f40e2
Farahrama <rafarah507@gmail.com>
2023-10-12 15:22:45 -0700

    farah

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 /**
- * print_strings -  prints strings passed to the function separed by separators
+ * print_strings - prints strings passed to the function separed by separators
  * @separator: A string that is used to separate strings
  * @n: number of number of unnamed parameters
  * Return: Always 0.
