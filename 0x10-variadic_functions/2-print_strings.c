79a7d747acc20147f18cccf8d5c32431d44829bc
Farahrama <rafarah507@gmail.com>
2023-10-12 15:36:43 -0700

    farah

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 /**
  * print_strings - prints strings passed to the function separed by separators
  * @separator: A string that is used to separate strings
- * @n: number of number of unnamed parameters
+ * @n: number of number of un named parameters
  *
  * Return: Always 0.
  */
@@ -10,13 +10,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 char *c;
 unsigned int i;
-va_list sep;
-va_start(sep, n);
+va_list ap;
+va_start(ap, n);
 if (separator == NULL)
 separator = "";
 for (i = 0; i < n; i++)
 {
-c = va_arg(sep, char*);
+c = va_arg(ap, char*);
 if (c == NULL)
 c = "(nil)";
 printf("%s", c);
@@ -24,5 +24,5 @@ if (i < n -1)
 printf("%s", separator);
 }
 printf("\n");
-va_end(sep);
+va_end(ap);
 }
