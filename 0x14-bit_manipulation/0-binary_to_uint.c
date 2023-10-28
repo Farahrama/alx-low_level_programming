cad51c70efa0c6d01aebcd145619114b0c9aa5c0
“[firstname <rafarah507@gmail.com>
2023-10-28 08:23:11 -0700

    y

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,22 +5,19 @@
  * @b: pointing to a string of 0 and 1 chars
  *
  * Return:converted number, or 0
- */
+*/
 unsigned int binary_to_uint(const char *b)
 {
 	int y;
 	unsigned int m;
-
 	m = 0;
 	if (!b)
 		return (0);
-	for (y = 0; b[y] != '\0'; y++)
-	{
+	for (y = 0; b[y] != '\0'; y++){
 		if (b[y] != '0' && b[y] != '1')
 			return (0);
 	}
-	for (y = 0; b[y] != '\0'; y++)
-	{
+	for (y = 0; b[y] != '\0'; y++){
 		m <<= 1;
 		if (b[y] == '1')
 			m += 1;
