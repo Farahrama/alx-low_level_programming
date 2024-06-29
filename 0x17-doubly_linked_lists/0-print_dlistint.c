131e0373595bee42478c7d660b2157ed1496cb7d
Farahrama <rafarah507@gmail.com>
2024-06-29 02:36:54 +0200

    g

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 size_t cout = 0;
 while (h != NULL)
 {
-	printf("%d\n", h->n);
-        h = h->next;
-	cout++;
+printf("%d\n", h->n);
+h = h->next;
+cout++;
 }
 return cout;
 }
