86361dc14bf92ced134521d8d32bf2abad171d8b
Farahrama <rafarah507@gmail.com>
2024-06-30 08:36:41 +0200

    j

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,6 +4,7 @@
 /**
  * sum_dlistint - returns the sum of all the data (n) of a dlistint_t
  * @head: Pointer to the head of the list
+ * Return: the sum of all the data (n) of a dlistint_t
  */
 int sum_dlistint(dlistint_t *head)
 {
