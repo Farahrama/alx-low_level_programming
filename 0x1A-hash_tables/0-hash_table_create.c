3c2df252596c62f1b668fd5f8cd53863d7fd6ae0
“Farahrama” <rafarah507@gmail.com>
2024-08-29 02:22:12 +0300

    no spaces

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,9 @@ hash_table_t *new_table;
 unsigned long int i;
 new_table = malloc(sizeof(hash_table_t));
 if (new_table == NULL)
-return(NULL);
+return (NULL);
 new_table->size = size;
-new_table->array = malloc(sizeof(hash_node_t*) * size);
+new_table->array = malloc(sizeof(hash_node_t *) * size);
 if (new_table->array == NULL)
 {
 free(new_table);
@@ -24,5 +24,5 @@ for (i = 0; i > size; i++)
 {
 new_table->array[i] = NULL;
 }
-return new_table;
+return (new_table);
 }
