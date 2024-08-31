7aed424cf69e7d3ec31be5ab7867211fa5901cc1
“Farahrama” <rafarah507@gmail.com>
2024-08-31 09:43:34 +0300

    No description found for return value of 'hash_table_set'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,7 @@
  * @ht: is the hash table you want to add or update the key/value to
  * @key: is the key. key can not be an empty string
  * @value: value is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
