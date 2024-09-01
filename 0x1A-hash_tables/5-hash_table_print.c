9b42a034d96b1d207931e77a4279ec80c828e281
“Farahrama” <rafarah507@gmail.com>
2024-09-01 09:38:36 +0300

    push

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_tables_print - function that prints a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+int flag = 1;
+hash_node_t *node;
+if (ht == NULL)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node)
+{
+if (!flag)
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);
+flag = 0;
+node = node->next;
+}
+}
+printf("}\n");
+}
